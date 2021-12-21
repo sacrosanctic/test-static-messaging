919052cc3d743d69525718568ea581efc80ac916
Scott Wu <sw@scottwu.ca>
2021-12-21 19:17:13 +0800

    add firebase and adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      pages: "build",
+      assets: "build",
+      fallback: null,
+    }),
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
+}
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
-
-export default config;
+export default config
